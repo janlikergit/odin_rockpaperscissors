db6e92fd19ef859a02de74d3304f9c5d2697c93a
janlikergit <89532473+janlikergit@users.noreply.github.com>
2022-11-27 23:23:12 +0000

    testing function getComputerChoice

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+let compChoice = Math.floor((Math.random()*2) + 1);
 
+function getComputerChoice(compChoice) {
+    if (compChoice == 1) {
+        compChoice = "Rock";
+    } else if (compChoice == 2) {
+        compChoice = "Paper";
+    } else if (compChoice == 3) {
+        compChoice = "Scissors";
+    }
+}
+
+console.log(compChoice);
