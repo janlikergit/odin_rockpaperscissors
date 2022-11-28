57ba9816196414e63b74c7ae3616580ee0c82c36
Jack <anliker.jack@gmail.com>
2022-11-27 19:11:23 -0500

    Function getComputerChoice successfully outputs a random choice
    git status

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+let compChoice = Math.floor(Math.random()*3) + 1;
 
+function getComputerChoice(compChoice) {
+    let result;
+    if (compChoice == 1) {
+        result = "Rock";
+    } else if (compChoice == 2) {
+        result = "Paper";
+    } else if (compChoice == 3) {
+        result = "Scissors";
+    }
+    return result;
+}
+
+console.log(getComputerChoice(compChoice));
