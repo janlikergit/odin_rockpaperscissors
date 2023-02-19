f2bc13e703a6c9589bf5d19b7b682d419f2d47f9
Jack <anliker.jack@gmail.com>
2023-02-18 21:16:41 -0500

    added script

--- script.js
+++ script.js
@@ -1,20 +1,54 @@
-let compChoice = Math.floor(Math.random()*3) + 1;
+let computerSelection
 
-function getComputerChoice(compChoice) {
-    let result;
-    if (compChoice == 1) {
-        result = "Rock";
-    } else if (compChoice == 2) {
-        result = "Paper";
-    } else if (compChoice == 3) {
-        result = "Scissors";
+function getComputerChoice() {
+    let x = Math.floor(Math.random()*3) + 1;
+    if (x == 1) {
+        computerSelection = "rock";
+    } else if (x == 2) {
+        computerSelection = "paper";
+    } else if (x == 3) {
+        computerSelection = "scissors";
     }
-    return result;
 }
 
-let playerSelection = window.prompt("Enter Rock, Paper, or Scissors");
-let computerSelection = getComputerChoice(compChoice);
+getComputerChoice(computerSelection);
+
+let playerSelection = (window.prompt("Enter Rock, Paper, or Scissors")).toLowerCase();
+
+function checkInput() {
+    if ((playerSelection !== "rock") && (playerSelection !== "paper") && (playerSelection !== "scissors")) {
+        alert("Please refresh the page and enter a valid choice.");
+    }
+}
+
+checkInput(playerSelection);
 
 function playRound(playerSelection, computerSelection) {
+    if ((playerSelection == "rock") && (computerSelection == "rock")) {
+        return "Tie. Play again.";
+    } else if ((playerSelection == "rock") && (computerSelection == "paper")) {
+        return "You Lose! Paper beats Rock.";
+    } else if ((playerSelection == "rock") && (computerSelection == "scissors")) {
+        return "You Win! Rock beats Scissors.";
+    } else if ((playerSelection == "paper") && (computerSelection == "paper")) {
+        return "Tie. Play again.";
+    } else if ((playerSelection == "paper") && (computerSelection == "scissors")) {
+        return "You Lose! Scissors beats Paper.";
+    } else if ((playerSelection == "paper") && (computerSelection == "rock")) {
+        return "You Win! Paper beats Rock.";
+    } else if ((playerSelection == "scissors") && (computerSelection == "scissors")) {
+        return "Tie. Play again.";
+    } else if ((playerSelection == "scissors") && (computerSelection == "rock")) {
+        return "You Lose! Rock beats Scissors.";
+    } else if ((playerSelection == "scissors") && (computerSelection == "paper")) {
+        return "You Win! Scissors beats Paper.";
+    }
+}
 
+console.log(playRound(playerSelection, computerSelection));
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        playRound();
+    };
 }
